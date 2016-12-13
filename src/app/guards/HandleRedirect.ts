f570a40ded25c81b34dea5bd3c3791f008f30903
Maxim Milovanov <mma29121983@gmail.com>
2016-12-12 22:54:19 -0500

    simple deployment check

--- src/app/guards/HandleRedirect.ts
+++ src/app/guards/HandleRedirect.ts
@@ -10,6 +10,7 @@ export class RedirectResolver implements Resolve<any> {
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
   ): Observable<any>|Promise<boolean> {
+    console.log('verify firebase deploy');
     return this.router.navigate([`/${AppPaths.courses.path}`, { outlets: { list: [AppPaths.courses.children.list.path] } }]);
   }
 }
